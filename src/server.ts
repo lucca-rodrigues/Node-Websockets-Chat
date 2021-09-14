4f065f8a5bbe5fdab470d22c9767ff206b21d3d6
admin <admin@MacBook-Pro-de-admin.local>
2021-09-14 13:28:21 -0300

    add congigs socket

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
-import express from "express"
+import express from "express";
+import path from "path";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const port = 3000;
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("Socket ID:", socket);
+})
 
 app.get("/", (req, res) => {
     return res.json({
