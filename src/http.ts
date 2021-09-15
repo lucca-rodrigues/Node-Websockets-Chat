d57180855bc12e8d385130d515a8017932a6bf1c
admin <admin@MacBook-Pro-de-admin.local>
2021-09-15 19:10:02 -0300

    create user service

--- src/http.ts
+++ src/http.ts
@@ -1,6 +1,7 @@
+import "reflect-metadata";
 import express from "express";
 import path from "path";
-import {createServer} from "http";
+import { createServer } from "http";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
 
@@ -9,8 +10,8 @@ const port = 3000;
 const server = createServer(app);
 
 mongoose.connect("mongodb://localhost/websocketcourse", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 app.use(express.static(path.join(__dirname, "..", "public")));
@@ -18,13 +19,13 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-    console.log("Socket ID:", socket);
-})
+  console.log("Socket ID:", socket);
+});
 
 app.get("/", (req, res) => {
-    return res.json({
-        message: "Hello  Websocket"
-    })
-})
+  return res.json({
+    message: "Hello  Websocket",
+  });
+});
 
-export {server, io, port};
+export { server, io, port };
