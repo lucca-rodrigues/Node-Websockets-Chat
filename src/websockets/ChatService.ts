e0c5eb4839754f2259ef52887a41c0f1c0a35e5e
admin <admin@MacBook-Pro-de-admin.local>
2021-09-22 20:34:04 -0300

    websockets list all users logged

--- src/websockets/ChatService.ts
+++ src/websockets/ChatService.ts
@@ -1,6 +1,7 @@
 import { container } from "tsyringe";
 import { io } from "../http";
 import { CreateUserUseService } from "../services/CreateUserService";
+import { GetAllUsersService } from "../services/GetAllUsersService";
 
 io.on("connect", (socket) => {
   socket.on("start", async (data) => {
@@ -15,5 +16,14 @@ io.on("connect", (socket) => {
     });
 
     console.log(user);
+
+    socket.broadcast.emit("new_users", user);
+
+    socket.on("get_users", async (callback) => {
+      const getAllUsersService = container.resolve(GetAllUsersService);
+      const users = await getAllUsersService.execute();
+
+      callback(users);
+    });
   });
 });
